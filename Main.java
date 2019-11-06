31d9c1ee30ec2969272c28077b3cf7962be104a6
Zaworov <Burntsandwich70!>
2019-11-07 00:28:31 +0100

    Tiny re-format

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import javax.sound.midi.MidiUnavailableException;
+
 public class Main {
-    public static void main (String[] args){
-        MusicTester musicTester = new MusicTester();
-        musicTester.play();
+    public static void main (String[] args) throws MidiUnavailableException {
+//        if (args.length < 2) throw new RuntimeException("You need to give at least two numerical arguments!");
+
+//        int instrument = Integer.parseInt(args[0]);
+        int instrument = 13;
+//        int note = Integer.parseInt(args[1]);
+        int note = 64;
+
+        MusicMachine musicMachine = new MusicMachine();
+        musicMachine.play(instrument, note);
     }
 }
---
+++ MusicMachine.java
@@ -0,0 +1,44 @@
+import org.jetbrains.annotations.NotNull;
+
+import javax.sound.midi.*;
+
+class MusicMachine {
+    private static final int NOTE_ON_COMMAND = 144;
+    private static final int NOTE_OFF_COMMAND = 128;
+
+    void play(int instrument, int note) throws MidiUnavailableException {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            Sequence sequence = new Sequence(Sequence.PPQ, 4);
+            Track track = sequence.createTrack();
+
+            changeInstrument(track, 13);
+
+            MidiEvent startNote = getNote(NOTE_ON_COMMAND);
+            MidiEvent endNote = getNote(NOTE_OFF_COMMAND);
+            track.add(startNote);
+            track.add(endNote);
+
+            sequencer.setSequence(sequence);
+            sequencer.start();
+        } catch (MidiUnavailableException | InvalidMidiDataException midiUnavailable) {
+            midiUnavailable.printStackTrace();
+        }
+    }
+
+    @NotNull
+    private MidiEvent getNote(int commandNo) throws InvalidMidiDataException {
+        ShortMessage messageA = new ShortMessage();
+        messageA.setMessage(commandNo, 1, 64, 100);
+        return new MidiEvent(messageA, 1);
+    }
+
+    private void changeInstrument(Track track, int instrument) throws InvalidMidiDataException {
+        ShortMessage changeMessage = new ShortMessage();
+        changeMessage.setMessage(192, 1, instrument, 0);
+        MidiEvent changeInstrument = new MidiEvent(changeMessage, 1);
+        track.add(changeInstrument);
+    }
+}
