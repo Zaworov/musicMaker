e2b5db21f77520d18fd852ea4c1c265183039948
Zaworov <Burntsandwich70!>
2019-11-09 17:55:43 +0100

    button made private

--- SimpleGUI.java
+++ SimpleGUI.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class SimpleGUI implements ActionListener{
-    JButton button;
+    private JButton button;
 
     void generateInterface(){
         JFrame frame = new JFrame();
