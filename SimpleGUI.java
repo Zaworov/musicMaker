22daebe1949161424784445a8ca34423801f43ba
Zaworov <Burntsandwich70!>
2019-11-09 17:55:21 +0100

    Action listener added Action handled

---
+++ SimpleGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class SimpleGUI implements ActionListener{
+    JButton button;
+
+    void generateInterface(){
+        JFrame frame = new JFrame();
+        button = new JButton("Push me!");
+        button.addActionListener(this);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been pushed!");
+    }
+}
