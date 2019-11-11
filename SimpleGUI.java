1e7a9d5f2b6cd53a3f472f3e04eb4c60e1c8031d
Zaworov <Burntsandwich70!>
2019-11-11 21:41:33 +0100

    added second button for text

--- SimpleGUI.java
+++ SimpleGUI.java
@@ -1,24 +1,26 @@
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class SimpleGUI implements ActionListener{
-    private JButton button;
+class SimpleGUI implements ActionListener {
+    private JButton colorButton, textButton;
     private JFrame frame;
+    private int counter = 0;
 
-    void generateInterface(){
+    void generateInterface() {
         frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        button = new JButton("Push me!");
-        button.addActionListener(this);
+        colorButton = new JButton("Change color!");
+        textButton = new JButton("PUSH IT!");
+        colorButton.addActionListener(this);
+        textButton.addActionListener(this);
 
         GraphicPanel graphicPanel = new GraphicPanel();
 
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.EAST, textButton);
         frame.getContentPane().add(BorderLayout.CENTER, graphicPanel);
         frame.setSize(300, 300);
         frame.setVisible(true);
@@ -26,7 +28,12 @@ class SimpleGUI implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        button.setText("I've been pushed!");
-        frame.repaint();
+        if (event.getSource() == colorButton) {
+            colorButton.setText("Change again!");
+            frame.repaint();
+        } else if (event.getSource() == textButton) {
+            counter++;
+            textButton.setText("PUSHED " + counter + " TIMES!");
+        }
     }
 }
