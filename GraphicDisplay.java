a0dd4062c45c80adfcccc15efc9b85c9db95b9a1
Zaworov <zavvorov@gmail.com>
2020-05-27 01:38:04 +0200

    Adding new class to display graphics

---
+++ GraphicDisplay.java
@@ -0,0 +1,12 @@
+//package out;
+
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.ShortMessage;
+
+public class GraphicDisplay implements ControllerEventListener {
+
+    @Override
+    public void controlChange(ShortMessage event) {
+
+    }
+}
